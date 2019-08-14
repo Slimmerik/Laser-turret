21ff7c980f0e20d99d011f37503f6018425694ed
Rik Honcoop <slimmerik@users.noreply.github.com>
2019-08-14 18:23:00 +0200

    added changes from pr review feedback

--- code/src/laser.cpp
+++ code/src/laser.cpp
@@ -1,6 +1,7 @@
 #include <laser.hpp>
 
-r2d2::laser::laser_c::laser_c(uint8_t gpio): gpio_port(gpio){
+r2d2::laser::laser_c::laser_c(uint8_t gpio)
+    : gpio_port(gpio){
     PIOB->PIO_OER = { 0x1U << gpio_port };
     PIOB->PIO_CODR = 0x01 << gpio_port;
 }
